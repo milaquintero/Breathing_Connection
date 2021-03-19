9dbff8c56d39b1427625efe7a469b1b9ece98eec
Mila <mquintero@SBD2.com>
2021-03-19 06:05:03 -0500

    added reusable fancy form page and refactored email sub page to use it

--- lib/pages/email_subscription_page.dart
+++ lib/pages/email_subscription_page.dart
@@ -1,5 +1,6 @@
 import 'package:breathing_connection/models/email_form_model.dart';
 import 'package:breathing_connection/styles.dart';
+import 'package:breathing_connection/widgets/fancy_form_page.dart';
 import 'package:breathing_connection/widgets/fancy_text_form_field.dart';
 import 'package:flutter/material.dart';
 class EmailSubscriptionPage extends StatefulWidget {
@@ -17,124 +18,67 @@ class _EmailSubscriptionPageState extends State<EmailSubscriptionPage> {
   Widget build(BuildContext context) {
     //screen height
     double screenHeight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        toolbarHeight: appBarHeight,
-        title: Text(
-            'Email Subscription',
-            style: appBarTextStyle,
-        ),
-        centerTitle: true,
-        backgroundColor: brandPrimary,
-      ),
-      body: Container(
-        color: Colors.blue[50],
-        padding: EdgeInsets.symmetric(horizontal: 36),
-        child: Stack(
-          clipBehavior: Clip.none,
-          alignment: Alignment.topCenter,
+    return FancyFormPage(
+      headerIcon: Icons.email,
+      headerColor: brandPrimary,
+      headerIconColor: Colors.grey[50],
+      form: Form(
+        key: _formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Positioned(
-                bottom: -200,
-                right: -240,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(300),
-                  child: Container(
-                    width: 400,
-                    height: 400,
-                    color: Colors.grey,
-                  ),
-                )
+            FancyTextFormField(
+              fieldLabel: 'Enter Name',
+              fieldType: 'name',
+              keyboardType: TextInputType.name,
+              onSaved: (name){
+                emailFormModel.name = name;
+              },
             ),
-            Positioned(
-                bottom: 0,
-                right: -280,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(300),
-                  child: Container(
-                    width: 400,
-                    height: 400,
-                    color: Colors.blue[200].withOpacity(0.4),
-                  ),
-                )
+            FancyTextFormField(
+                fieldLabel: 'Enter Email',
+                fieldType: 'email',
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (email){
+                  emailFormModel.email = email;
+                }
             ),
-            ListView(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 16, bottom: 12),
-                  child: CircleAvatar(
-                    backgroundColor: brandPrimary,
-                    radius: screenHeight / 12,
-                    child: Icon(
-                      Icons.email,
-                      size: screenHeight / 12,
-                      color: Colors.grey[50],
+            FancyTextFormField(
+                fieldLabel: 'Enter Birth Date',
+                fieldType: 'date',
+                keyboardType: TextInputType.datetime,
+                onSaved: (birthDate){
+                  emailFormModel.birthDate = birthDate;
+                }
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 36, bottom: 28),
+              child: TextButton(
+                onPressed: (){
+                  //validate form
+                  if(_formKey.currentState.validate()){
+                    //store valid entries into model
+                    _formKey.currentState.save();
+                    //TODO: send request to add user to email list
+                  }
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 2),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24
                     ),
                   ),
                 ),
-                Form(
-                  key: _formKey,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      FancyTextFormField(
-                        fieldLabel: 'Enter Name',
-                        fieldType: 'name',
-                        keyboardType: TextInputType.name,
-                        onSaved: (name){
-                          emailFormModel.name = name;
-                        },
-                      ),
-                      FancyTextFormField(
-                        fieldLabel: 'Enter Email',
-                        fieldType: 'email',
-                        keyboardType: TextInputType.emailAddress,
-                        onSaved: (email){
-                          emailFormModel.email = email;
-                        }
-                      ),
-                      FancyTextFormField(
-                        fieldLabel: 'Enter Birth Date',
-                        fieldType: 'date',
-                        keyboardType: TextInputType.datetime,
-                        onSaved: (birthDate){
-                          emailFormModel.birthDate = birthDate;
-                        }
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 36, bottom: 28),
-                        child: TextButton(
-                          onPressed: (){
-                            //validate form
-                            if(_formKey.currentState.validate()){
-                              //store valid entries into model
-                              _formKey.currentState.save();
-                              //TODO: send request to add user to email list
-                            }
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 2),
-                            child: Text(
-                              'Sign Up',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 24
-                              ),
-                            ),
-                          ),
-                          style: TextButton.styleFrom(
-                              backgroundColor: brandPrimary
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                style: TextButton.styleFrom(
+                    backgroundColor: brandPrimary
                 ),
-              ],
-            ),
+              ),
+            )
           ],
         ),
       ),
---
+++ lib/widgets/fancy_form_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import '../styles.dart';
+import 'fancy_text_form_field.dart';
+
+class FancyFormPage extends StatefulWidget {
+  final Form form;
+  final Color headerColor;
+  final Color headerIconColor;
+  final IconData headerIcon;
+  FancyFormPage({this.form, this.headerIcon, this.headerColor, this.headerIconColor});
+  @override
+  _FancyFormPageState createState() => _FancyFormPageState();
+}
+
+class _FancyFormPageState extends State<FancyFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    //screen height
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        toolbarHeight: appBarHeight,
+        title: Text(
+          'Email Subscription',
+          style: appBarTextStyle,
+        ),
+        centerTitle: true,
+        backgroundColor: brandPrimary,
+      ),
+      body: Container(
+        color: Colors.blue[50],
+        padding: EdgeInsets.symmetric(horizontal: 36),
+        child: Stack(
+          clipBehavior: Clip.none,
+          alignment: Alignment.topCenter,
+          children: [
+            Positioned(
+                bottom: -200,
+                right: -240,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(300),
+                  child: Container(
+                    width: 400,
+                    height: 400,
+                    color: Colors.grey,
+                  ),
+                )
+            ),
+            Positioned(
+                bottom: 0,
+                right: -280,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(300),
+                  child: Container(
+                    width: 400,
+                    height: 400,
+                    color: Colors.blue[200].withOpacity(0.4),
+                  ),
+                )
+            ),
+            ListView(
+              children: [
+                //icon header
+                Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 12),
+                  child: CircleAvatar(
+                    backgroundColor: widget.headerColor,
+                    radius: 49.34,
+                    child: Icon(
+                      widget.headerIcon,
+                      size: 49.34,
+                      color: widget.headerIconColor,
+                    ),
+                  ),
+                ),
+                //add form after icon header
+                widget.form
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
